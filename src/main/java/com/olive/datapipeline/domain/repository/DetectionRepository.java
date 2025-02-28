16db08c48a296fb7ce47ecd3001a822ea8f55140
Oleksii Tsvietaiev <o.tsvietaiev92@gmail.com>
2025-02-28 16:39:13 +0200

    refactoring(model): remove all functions

--- src/main/java/com/olive/datapipeline/domain/repository/DetectionRepository.java
+++ src/main/java/com/olive/datapipeline/domain/repository/DetectionRepository.java
@@ -4,10 +4,5 @@ import com.olive.datapipeline.domain.model.Detection;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-import java.util.UUID;
-
 public interface DetectionRepository extends MongoRepository<Detection, ObjectId> {
-
-    Optional<Detection> findByGuid(UUID guid);
 }
